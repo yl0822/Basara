9d152ac074118f6fd53013adee7295ede4432488
杨龙 <long.yl@corp.netease.com>
2016-01-11 16:06:52 +0800

    测试SSH

--- bad-coder-2015/src/main/java/BootStrap.java
+++ bad-coder-2015/src/main/java/BootStrap.java
@@ -1,9 +1,22 @@
+import java.util.Arrays;
+
 /**
  * @author long.yl.
  */
 public class BootStrap {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str + " " + "asdasd");
+        String str = "1035194-35056-1033016_1055064-35057-0_1055062-35057-0_1036621-38012-0_1065414-35041-0_1065414-35041-0_1035193-35056-0_1035195-35056-0_1055063-35057-0_1063006-35057-0_1062012-35060-0_1035195-35056-0_1035193-35056-0_1065414-35041-0_1055059-35057-0_1035208-35057-0_1035208-35057-0_1055059-35057-0_1055126-35054-0_1055128-35054-0_1035193-35056-0_1035195-35056-0_1055063-35057-0_1062012-35060-0_1063006-35057-0_1035195-35056-0";
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println(sb.toString());
+        String addedID = "1036627-38014-0";
+        String[] prdtIds = sb.toString().split("_");
+        if (Arrays.asList(prdtIds).contains(addedID)){
+            if (sb.toString().startsWith(addedID)){
+                sb = new StringBuilder(sb.toString().replace(addedID + "_", ""));
+            }else {
+                sb = new StringBuilder(sb.toString().replace("_" + addedID, ""));
+            }
+        }
+        System.out.println(sb.toString());
    }
 }
---
+++ bad-coder-2015/src/main/java/algorithm/netease/basara/sort/Timsort.java
@@ -0,0 +1,26 @@
+package algorithm.netease.basara.sort;
+
+import java.util.*;
+
+/**
+ * @author long.yl.
+ * @Date 2016/1/6
+ */
+public class Timsort {
+    public static void main(String[] args) {
+        int[] sample = new int[]
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-2,1,0,-2,0,0,0,0};
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i : sample)
+            list.add(i);
+            // use the native TimSort in JDK 7
+        Collections.sort(list, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                // miss the o1 = o2 case on purpose
+                return o1 > o2 ? 1 : -1;
+            }
+        });
+    }
+}
