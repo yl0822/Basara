a30ff3d13cb3de1215c0109162da76cc9de07a08
杨龙 <long.yl@corp.netease.com>
2016-01-14 14:10:58 +0800

    新增http类

---
+++ bad-coder-2015/src/main/java/tools/netease/basara/MapBasara.java
@@ -0,0 +1,13 @@
+package tools.netease.basara;
+
+import java.util.Map;
+
+/**
+ * @author long.yl.
+ * @Date 2016/1/14
+ */
+public class MapBasara {
+    public static boolean isNullOrEmpty(Map<?, ?> map){
+        return map == null || map.isEmpty();
+    }
+}
