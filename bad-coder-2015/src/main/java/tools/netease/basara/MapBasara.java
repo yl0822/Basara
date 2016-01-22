d9579c7cf63a992be02f74496bfbab9490b19473
杨龙 <long.yl@corp.netease.com>
2016-01-22 13:32:51 +0800

    一些权限验证的修改

--- bad-coder-2015/src/main/java/tools/netease/basara/MapBasara.java
+++ bad-coder-2015/src/main/java/tools/netease/basara/MapBasara.java
@@ -10,4 +10,21 @@ public class MapBasara {
     public static boolean isNullOrEmpty(Map<?, ?> map){
         return map == null || map.isEmpty();
     }
+
+    public static void print(Map<String, ?> map){
+        if (isNullOrEmpty(map)){
+            return;
+        }
+        StringBuilder sb = new StringBuilder("start to print map ,size : ").append(map.size()).append(" as follow....");
+        int num = 0;
+        for (String key : map.keySet()) {
+            sb.append(System.getProperties().get("line.separator"));
+            sb.append(num++);
+            sb.append(" key: ");
+            sb.append(key);
+            sb.append(", value: ");
+            sb.append(map.get(key));
+        }
+        System.out.println(sb.toString());
+    }
 }
--- bad-coder-2015/src/test/java/algorithm/netease/basara/test/BootStrap.java
+++ bad-coder-2015/src/test/java/algorithm/netease/basara/test/BootStrap.java
@@ -1,8 +1,30 @@
 package algorithm.netease.basara.test;
 
+import mode.netease.basara.test.Book;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author long.yl.
  * @Date 2015/12/31
  */
 public class BootStrap {
+    public static void main(String[] args) {
+        List<Book> stringList = new ArrayList<>();
+        Book book = new Book();
+        book.setName("asc");
+        stringList.add(book);
+        BootStrap strap = new BootStrap();
+        System.out.println(stringList.get(0).getName());
+        strap.changeList(stringList);
+        System.out.println(stringList.get(0).getName());
+    }
+
+    public void changeList(List<Book> stringList){
+        for (Book book : stringList) {
+            book.setName("asc__sd");
+        }
+    }
 }
