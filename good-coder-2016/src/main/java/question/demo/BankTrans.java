27c74d3e9d3dd671c93ef7dbcc7bd9909790e762
杨龙 <sdstyle0822@gmail.com>
2016-06-21 21:48:48 +0800

    file reader

---
+++ good-coder-2016/src/main/java/question/demo/BankTrans.java
@@ -0,0 +1,106 @@
+package question.demo;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author long.yl.
+ * @Date 2016/6/21
+ */
+public class BankTrans {
+    static class User{
+        private String name;
+        private int money;
+
+        public User(String name, int money) {
+            this.name = name;
+            this.money = money;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getMoney() {
+            return money;
+        }
+
+        public void setMoney(int money) {
+            this.money = money;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "name='" + name + '\'' +
+                    ", money=" + money +
+                    '}';
+        }
+    }
+    private static Map<String, User> registeredUser;
+
+    public static AtomicBoolean lock = new AtomicBoolean(false);
+
+    public static void init(){
+        registeredUser.put("larry", new User("larry", 15000));
+        registeredUser.put("tom", new User("tom", 10000));
+    }
+
+    static{
+        registeredUser = new ConcurrentHashMap<>();
+        init();
+    }
+
+    public boolean isBankUser(String name){
+        return registeredUser.get(name) != null;
+    }
+
+    public boolean isFinancialHealth(String name, int money){
+        return registeredUser.get(name).getMoney() >= money;
+    }
+
+    public boolean transMoney2(String fromName, String toName, int money){
+        boolean ret = false;
+        if (isBankUser(fromName) && isBankUser(toName)){
+            if (isFinancialHealth(fromName, money)){
+                Map<String, User> registeredUser_ = new ConcurrentHashMap<>(registeredUser);
+                // 利用lock来防止共享变量一致性问题
+                synchronized (lock){
+                    try {
+                        registeredUser_.put(fromName, new User(fromName, registeredUser_.get(fromName).getMoney() - money));
+                        System.out.println(new String[]{"asd", "asd"}[45]);
+                        registeredUser_.put(toName, new User(toName, registeredUser_.get(toName).getMoney() + money));
+                        ret = true;
+                        // 利用临时Map来考虑事务
+                        registeredUser = registeredUser_;
+                    }catch (Exception e){
+                        System.out.println("some exception : "+ e.getLocalizedMessage()+" occurs during money transing!rollback");
+                    }
+                }
+            }else {
+                System.out.println("from user who don't have enough money!");
+            }
+        }else {
+            System.out.println("someone may not our bank register user!");
+        }
+        return ret;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(registeredUser.values().toArray());
+    }
+
+    public static void main(String[] args) {
+        BankTrans trans = new BankTrans();
+        System.out.println(trans);
+        trans.transMoney2("larry", "tom", 2500);
+        System.out.println(trans);
+    }
+}
---
+++ good-coder-2016/src/main/java/question/demo/RecursionFileReader.java
@@ -0,0 +1,30 @@
+package question.demo;
+
+import java.io.*;
+import java.util.HashMap;
+
+/**
+ * @author long.yl.
+ * @Date 2016/6/21
+ */
+public class RecursionFileReader {
+    public static void readFile(File file){
+        if (file.isDirectory()){
+            for (File file1 : file.listFiles()){
+                readFile(file1);
+            }
+        }else {
+            System.out.println("Opt file "  + file.getName());
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        Reader reader = new InputStreamReader(new FileInputStream(new File("d:\\config")));
+        Reader reader1 = new FileReader(new File(""));
+
+        Writer writer = new OutputStreamWriter(new FileOutputStream(new File("d:\\")));
+        Writer writer1 = new FileWriter(new File(""));
+
+        RecursionFileReader.readFile(new File("d:\\config"));
+    }
+}
