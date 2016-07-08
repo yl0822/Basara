5f0ab2079f46acc46725cda06c547fd8ccc35286
杨龙 <sdstyle0822@gmail.com>
2016-07-08 19:36:03 +0800

    乱七八糟的东西3

---
+++ springmvc/src/main/java/com/basara/dao/MybatisTest.java
@@ -0,0 +1,21 @@
+package com.basara.dao;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+
+/**
+ * Created by long.yl on 2016/7/8.
+ */
+public class MybatisTest {
+    public static void main(String[] args) throws Exception{
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        sqlSession.selectList("SELECT * FROM STUDENTS");
+    }
+}
