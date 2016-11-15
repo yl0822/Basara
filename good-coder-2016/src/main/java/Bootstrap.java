3ca14bb99eac902f2c7365cc83eeb7b07a2f791a
杨龙 <sdstyle0822@gmail.com>
2016-11-15 10:57:09 +0800

    难得的清闲时间

--- good-coder-2016/src/main/java/Bootstrap.java
+++ good-coder-2016/src/main/java/Bootstrap.java
@@ -1,10 +1,8 @@
 import com.alibaba.fastjson.JSON;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Created by Larry .Yang
@@ -13,46 +11,14 @@ import java.util.Map;
  */
 public class Bootstrap {
     public static void main(String[] args) throws Exception {
-        List<Student> list = new ArrayList<>();
-        list.add(new Student("aaa", 23));
-        list.add(new Student("bbb", 22));
-        list.add(new Student("ccc", 31));
-        list.add(new Student("ddd", 34));
-        Map<String, String> map = new HashMap<>();
-        map.put("aaa", "aaaaaa");
-        map.put("bbb", "bbbbbb");
-        map.put("ccc", "cccccc");
-        map.put("ddd", "dddddd");
-        Map<String, Object> result = new HashMap<>();
-        result.put("result", map);
-        Map<String, Object> result2 = new HashMap<>();
-        result2.put("result", list);
-//        System.out.println(JSON.toJSONString(result));
-        System.out.println(JSON.toJSONString(result2));
+        Calendar calendar = Calendar.getInstance();
+        // 保留一个月
+        calendar.add(Calendar.DATE, -30);
+        String time1 = new SimpleDateFormat("yyyy-MM-dd 00:00:00").format(calendar.getTime());
+        calendar.add(Calendar.DATE, 1);
+        String time2 = new SimpleDateFormat("yyyy-MM-dd 00:00:00").format(calendar.getTime());
+        String sql = "SELECT Id FROM Musician_SongAudit where db_create_time < '"+time2+"' and db_create_time > '" + time1 +"'";
+        System.out.println(sql);
     }
 }
-class Student{
-    private String name;
-    private int age;
 
-    public Student(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-}
